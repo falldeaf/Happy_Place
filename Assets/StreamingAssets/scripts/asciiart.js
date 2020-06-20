9f696bd4777632ba1ad7fa1a286dca9a49230bcd
falldeaf <falldeaf@gmail.com>
2020-06-20 15:08:48 -0400

    ascii art gallery program!

---
+++ Assets/StreamingAssets/scripts/asciiart.js
@@ -0,0 +1,62 @@
+setup("oggplayer")
+led("purple")
+write1("")
+write2("Welcome to the ascii Art gallery! \\(^O^)/\n\nClick left or right to view ASCII Art.")
+
+art_index = 0
+artlist = [
+"\
+ ____ ___   jgs\n\
+ )  =\\  =\\\n\
+/    =\\  =\\\n\
+\\      `-._`-._\n\
+ )__(`\\____)___)",
+
+"   _.........._\n\
+  | |mga     | | \n\
+  | |        | | \n\
+  | |________| | \n\
+  |   ______   | \n\
+  |  |    | |  | \n\
+  |__|____|_|__| ",
+
+"      ____ \n\
+    ,'   Y`. \n\
+   /        \\ \n\
+   \\ ()  () / \n\
+    `. /\\ ,' \n\
+8====| '' |====8\n\
+     `LLLU'",
+
+"      ,~~. \n\
+     (  9 )-_,\n\
+(\\___ )=='-'\n\
+ \\ .   ) )\n\
+  \\ `-' /\n\
+   `~j-'   hjw\n\
+     '=:",
+
+"  A_A\n\
+ (-.-)\n\
+  |-|\n\
+ /   \\\n\
+|     | \n\
+|  || |  /\\__\n\
+ \\_||_/_/"
+]
+
+function action(a) {
+	log(a)
+	switch(a) {
+		case "left_click":
+			art_index--;
+			if(art_index<0) art_index = artlist.length-1
+			write1(artlist[art_index])
+			break
+		case "right_click":
+			art_index++;
+			if(art_index>artlist.length-1) art_index = 0
+			write1(artlist[art_index])
+			break
+	}
+}
